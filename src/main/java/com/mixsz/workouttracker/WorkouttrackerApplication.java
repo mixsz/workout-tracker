f4e276c7e6197afbb7605b5c18b7eeb7ace4159e
Danilo <danilopradella@hotmail.com>
2026-06-27 19:03:10 -0300

    configs do ambiente

--- src/main/java/com/mixsz/workouttracker/WorkouttrackerApplication.java
+++ src/main/java/com/mixsz/workouttracker/WorkouttrackerApplication.java
@@ -1,5 +1,6 @@
 package com.mixsz.workouttracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkouttrackerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
 		SpringApplication.run(WorkouttrackerApplication.class, args);
+
 	}
 
 }
