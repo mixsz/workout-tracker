496195699485109520cae4aa1fd05ced73381331
Danilo <danilopradella@hotmail.com>
2026-06-27 18:48:31 -0300

    configuracao inicial

---
+++ src/main/java/com/mixsz/workouttracker/WorkouttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mixsz.workouttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkouttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkouttrackerApplication.class, args);
+	}
+
+}
