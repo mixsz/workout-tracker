80e78e7e11bb5bd35141b30b7a4b014f3d2591a8
Danilo <danilopradella@hotmail.com>
2026-07-30 01:19:33 -0300

    adicionado atributo de traducao de muscleGroup no response exercise

--- src/main/java/com/mixsz/workouttracker/dto/response/ExerciseResponseDTO.java
+++ src/main/java/com/mixsz/workouttracker/dto/response/ExerciseResponseDTO.java
@@ -4,5 +4,9 @@ import com.mixsz.workouttracker.enums.MuscleGroup;
 
 import java.util.UUID;
 
-public record  ExerciseResponseDTO(UUID id, String name, MuscleGroup muscleGroup) {
+public record ExerciseResponseDTO(UUID id, String name, MuscleGroup muscleGroup, String muscleGroupLabel) {
+
+    public ExerciseResponseDTO(UUID id, String name, MuscleGroup muscleGroup) {
+        this(id, name, muscleGroup, muscleGroup.getDisplayName());
+    }
 }
